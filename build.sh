f42de3c3ab54f655e9ec70e795193d644f3aba98
Anurag Goel <anurag@render.com>
2019-01-21 11:51:56 -0800

    Create a build script to use in production on Render

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+export MIX_ENV=prod
+
+export APP_NAME="$(grep 'app:' mix.exs | sed -e 's/\[//g' -e 's/ //g' -e 's/app://' -e 's/[:,]//g')"
+export APP_VSN="$(grep 'version:' mix.exs | cut -d '"' -f2)"
+
+# remove existing builds
+rm -rf "_build"
+
+# Compile app and assets
+mix deps.get --only prod
+mix compile
+cd assets && npm install && npm run deploy && cd ..
+
+# create release
+mix do phx.digest, release --env=prod --no-tar
+
+echo
+echo "Linking release $APP_NAME:$APP_VSN to _render/"
+
+rm -rf _render
+ln -s "_build/$MIX_ENV/rel/$APP_NAME" _render
