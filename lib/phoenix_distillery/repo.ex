8dc9a9b39fd90261578accfe09c411d861008ae3
Anurag Goel <anurag@render.com>
2019-01-21 11:53:07 -0800

    Configure Ecto to use DATABASE_URL from the runtime env

--- lib/phoenix_distillery/repo.ex
+++ lib/phoenix_distillery/repo.ex
@@ -1,5 +1,10 @@
 defmodule PhoenixDistillery.Repo do
   use Ecto.Repo,
     otp_app: :phoenix_distillery,
-    adapter: Ecto.Adapters.Postgres
+    adapter: Ecto.Adapters.Postgres,
+    pool_size: 10
+
+  def init(_type, config) do
+    {:ok, Keyword.put(config, :url, System.get_env("DATABASE_URL"))}
+  end
 end
