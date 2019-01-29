a631d292b1cb3e247f0b9a26058a4a02ae69fc40
Anurag Goel <anurag@render.com>
2019-01-28 17:45:09 -0800

    Add a default secret key base

--- rel/config/config.exs
+++ rel/config/config.exs
@@ -2,7 +2,10 @@ use Mix.Config
 
 port = String.to_integer(System.get_env("PORT") || "4000")
 
+default_secret_key_base = :crypto.strong_rand_bytes(48) |> Base.encode64
+secret_key_base = System.get_env("SECRET_KEY_BASE") || default_secret_key_base
+
 config :phoenix_distillery, PhoenixDistilleryWeb.Endpoint,
   http: [port: port],
   url: [host: "localhost", port: port],
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: secret_key_base
